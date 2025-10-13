a76a49e29a3cf71bda9e6e3380ef40300911a507
Vaibhav-ks <vaibhavkshashwat2713@gmail.com>
2025-10-13 19:23:22 +0530

    allowed github pages url in url

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -39,7 +39,8 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:5500',
     'http://127.0.0.1:3000',  // Add this
-    'http://localhost:3000'
+    'http://localhost:3000',
+    `${process.env.CORS_ORIGIN}`
 ];
 app.use(cors({
     origin: function (origin, callback) {
